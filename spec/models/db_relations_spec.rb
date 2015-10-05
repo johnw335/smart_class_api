b40d516b84d4eda8bbf87239f070300f59b2895e
John Williams <jww335@gmail.com>
2015-10-05 13:31:21 -0400

    adding travis ci and some tests

--- spec/models/db_relations_spec.rb
+++ spec/models/db_relations_spec.rb
@@ -1,3 +1,17 @@
 require 'rails_helper'
 
-describe
+describe 'teacher' do
+
+  let(:teacher1) { Teacher.create!(email: "test1@teacher.com",
+              password: "password") }
+
+
+  it 'should have a new teacher with email test1@teacher.com' do
+      expect(teacher1.email).to eq("test1@teacher.com")
+  end
+
+  it 'should have a new teacher with password (password)' do
+      expect(teacher1.password).to eq("password")
+  end
+
+end
