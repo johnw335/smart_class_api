99d1e3ac35b5252a0c247b1d5e05876515402b86
John Williams <jww335@gmail.com>
2015-10-14 14:55:07 -0400

    figured out problem

--- app/controllers/attendance_controller.rb
+++ app/controllers/attendance_controller.rb
@@ -5,22 +5,13 @@ class AttendanceController < ApplicationController
   def create
     p params
     puts "params email"
-
-
     puts params[:email]
     puts "params class"
     puts params[:myClass]
-
     @studentEmail = params[:email]
     @userClass = params[:myClass]
-
-
     myStudent = Student.find_by(email: @studentEmail)
-    puts myStudent
     myClass = Classname.find_by(name: @userClass)
-    puts myClass
-
-
     if myStudent == nil || myClass == nil
       puts "one of those was nil"
       render :json => "Sorry, either class or student is incorrect"
@@ -31,14 +22,6 @@ class AttendanceController < ApplicationController
         puts "There was student data passed here"
         render :json => "200"
       else
-        # puts "I didnt make an attendance for some reason"
-        # puts "***** student *****"
-        # puts myStudent
-        # puts "***** student *****"
-        # puts "***** class *****"
-        # puts myClass
-        # puts "***** class *****"
-
         render :json => "Sorry I couldn't make an attendance"
      end
    end
