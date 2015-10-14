a01d168affc37b6744de6758333459a1faa77cff
John Williams <jww335@gmail.com>
2015-10-14 14:07:26 -0400

    still fixing bug

--- app/controllers/attendance_controller.rb
+++ app/controllers/attendance_controller.rb
@@ -5,31 +5,37 @@ class AttendanceController < ApplicationController
   def create
     p params
     @studentEmail = params[:email]
-    puts "***********"
-    puts @studentEmail
-    puts "***********"
+    # puts "***********"
+    # puts @studentEmail
+    # puts "***********"
     @userClass = params[:myClass]
-    puts "*****userClass"
-    puts @userClass
+    # puts "*****userClass"
+    # puts @userClass
 
     myStudent = Student.find_by(email: @studentEmail)
     myClass = Classname.find_by(name: @userClass)
 
+
+    if myStudent == nil || myClass == nil
+      puts "one of those was nil"
+      render :json => "Sorry, either class or student is incorrect"
+    else
     @newAttendance = Attendance.create!(student: myStudent, classname: myClass)
 
-    if @newAttendance != nil
-      puts "There was student data passed here"
-      render :json => "200"
-    else
-      puts "I didnt make an attendance for some reason"
-      puts "***** student *****"
-      puts myStudent
-      puts "***** student *****"
-      puts "***** class *****"
-      puts myClass
-      puts "***** class *****"
+      if @newAttendance != nil
+        puts "There was student data passed here"
+        render :json => "200"
+      else
+        puts "I didnt make an attendance for some reason"
+        puts "***** student *****"
+        puts myStudent
+        puts "***** student *****"
+        puts "***** class *****"
+        puts myClass
+        puts "***** class *****"
 
-      render :json => "Sorry I couldn't make an attendance"
+        render :json => "Sorry I couldn't make an attendance"
+     end
    end
 
 
